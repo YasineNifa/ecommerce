692ba6cba9b242afbde4855e0041e55433586243
Yassine Nifa <yasine.nifa@gmail.com>
2020-10-01 16:18:33 +0200

    create data category and data product2

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -3,6 +3,7 @@ from .views import home,productPage
 
 urlpatterns = [
     path('', home, name = 'home'),
+    path('<slug:category_slug>',home,name = 'product_by_category'),
     path('product/',productPage,name = 'product')
 
 ]
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 # from django.http import HttpResponse
-
+from .models import Product, Category
 # Create your views here.
 
-def home(request):
-	return render(request,'home.html',{})
+def home(request, category_slug=None):
+	category_page = None
+	products = None
+	if category_slug != None:
+		category_page = get_object_or_404(Category,slug=category_slug)
+		products = Product.objects.filter(category= category_page, available=True)
+
+	else :
+		products = Product.objects.all().filter(available = True)
+
+	context = {
+		'category' : category_page,
+		'products' : products
+	}
+	return render(request,'home.html',context)
+
+
+
 
 def productPage(request):
 	return render(request,'product.html',{})
