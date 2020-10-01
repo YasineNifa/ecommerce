1706b061c8d2a4788b26de70065154e2f6650315
Yassine Nifa <yasine.nifa@gmail.com>
2020-10-01 15:32:22 +0200

    create data category and data product

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Category(models.Model):
+	name = models.CharField(max_length=250, unique=True)
+	slug = models.SlugField(max_length=250, unique=True)# it is a part of the url which identifies a particular pageand a web site in a form readable by users
+	description = models.TextField(blank=True)
+	image = models.ImageField(upload_to='category', blank=True)
+
+
+	class Meta:
+		ordering = ('name',)
+		verbose_name = 'category'
+		verbose_name_plural = 'categories'
+
+	def __str__(self):
+		return self.name
+
+# Model: Product
+
+
+class Product(models.Model):
+	name = models.CharField(max_length=250, unique=True)
+	slug = models.SlugField(max_length=250, unique=True)
+	description = models.TextField(blank=True)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)#MANY TO ONE RELATIONSHIP every product is associeted to one category
+	price = models.DecimalField(max_digits=10, decimal_places=2)
+	image = models.ImageField(upload_to='product', blank=True)
+	stock = models.IntegerField()
+	available = models.BooleanField(default=True)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		ordering = ('name',)
+		verbose_name = 'product'
+		verbose_name_plural = 'products'
+
+	def __str__(self):
+		return self.name# for a nice readable presentation of a model
